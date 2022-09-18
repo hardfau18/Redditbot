4ebf3f91b846adacf1e0c045abac74d2a44cd812
pratiksha <pratikshaprabhu211@gmail.com>
2022-09-19 00:15:48 +0530

    e2e enabled

--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@
 
 import simplematrixbotlib as botlib
 import os
+import random
 
 server = os.environ.get("MATRIX_SERVER")
 user_name = os.environ.get("REDITBOTUNAME")
 password = os.environ.get("REDITPASSWORD")
+count = 0
+
+config = botlib.Config()
+config.encryption_enabled = True
+config.store_path = './crypto_store/'
 
 creds = botlib.Creds(server, user_name, password)
-bot = botlib.Bot(creds)
+bot = botlib.Bot(creds, config)
 PREFIX = '!'
 
 def ech(msg:str)->str:
@@ -19,10 +25,18 @@ def ech(msg:str)->str:
 
 @bot.listener.on_message_event
 async def echo(room, message):
+    global count
     match = botlib.MessageMatch(room, message, bot, PREFIX)
 
-    if match.is_not_from_this_bot() and match.prefix() and match.command("echo"):
-        msg = ech(" ".join(arg for arg in match.args()))
-        await bot.api.send_text_message(room.room_id, msg)
-
+    if match.is_not_from_this_bot():
+        if match.prefix():
+            if count <= 0:
+                msg = ech(message.body[1:])
+                await bot.api.send_text_message(room.room_id, msg)
+            else:
+                count -= 1
+                await bot.api.send_text_message(room.room_id, "🙊")
+        elif match.contains("shutup bot"):
+            count = random.randint(1,11)
+            await bot.api.send_text_message(room.room_id, "😥")
 bot.run()
